dff9ee004c76fdeab0aaf780b4b00ae67e39a7f9
UjwalVarma <ujwalvarma954@gmail.com>
2024-05-04 12:03:29 +0530

    movielist

---
+++ src/components/GenreFilter.js
@@ -0,0 +1,21 @@
+// GenreFilter.js
+import React from 'react';
+
+function GenreFilter({ genres }) {
+  const handleClick = (genre) => {
+    console.log(`Filtering by ${genre}`);
+  };
+
+  return (
+    <div className="genre-filter">
+      <h2>Filter by Genre</h2>
+      <div className="buttons">
+        {genres.map((genre, index) => (
+          <button key={index} onClick={() => handleClick(genre)}>{genre}</button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default GenreFilter;
